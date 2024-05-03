d23195644d74bce91aa0ea0f09468e833504627c
darishSFIT <122771348+darishSFIT@users.noreply.github.com>
2024-05-04 00:03:53 +0530

    Arduino code

--- parking.ino
+++ parking.ino
@@ -1,109 +1,134 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
-Servo myServo;
-bool gateOpen = false; // Flag to track if the gate is open
 
-SoftwareSerial bluetooth(8, 9); // RX, TX
+// Define pin numbers for IR sensors
+const int irSensorPin1 = 3;
+const int irSensorPin2 = 2;
+const int irSensorPin3 = 4;
+const int irSensorPin4 = 5; //gate IR
 
-const int irSensorPin = 5; // IR Sensor pin
-
-int irSensorPin1 = 2;
-int irSensorPin2 = 3;
-int irSensorPin3 = 4;
-int irSensorPin4 = 5;
-
-int irSensorValue1;
+// Define IR sensor values variable to store value of IR sensor state
+int irSensorValue1; 
 int irSensorValue2;
 int irSensorValue3;
-int irSensorValue4;
+int irSensorValue4; //gate IR
+
+// Create instances of Servo and SoftwareSerial
+Servo myServo;
+SoftwareSerial bluetooth(8, 9); //bluetooth RX, TX
+
+// Function prototypes
+void checkAndSendSlotStatus(int irSensorValue, const char* slotName);
 
 void setup() {
-  Serial.begin(9600);
-  bluetooth.begin(9600); // Default baud rate for most HC-05 modules
-  pinMode(irSensorPin, INPUT);  // Initialize IR sensor pin
-  //myServo.attach(10); // Attach the servo to pin 10
-  //myServo.write(0);  // Close the gate initially
-  //delay(100);
-  //myServo.detach();
+  Serial.begin(9600); // setting serial communication baud rate
+  bluetooth.begin(38400); // setting bluetooth baud rate
+  pinMode(irSensorPin4, INPUT); // IR sensor 4 pin as input (gate)
 }
 
 void loop() {
+  // Read values of all four IR sensors which are connected to digital pins
   irSensorValue1 = digitalRead(irSensorPin1);
   irSensorValue2 = digitalRead(irSensorPin2);
   irSensorValue3 = digitalRead(irSensorPin3);
   irSensorValue4 = digitalRead(irSensorPin4);
 
-  // If obstacle is detected
-  if (irSensorValue4 == HIGH) {
-      // Print the state of the IR sensor
-      Serial.println("No Entry detected\n");
-      bluetooth.println("No Entry detected\n");
-  }else{
-     Serial.println("Entry detected");
-      bluetooth.println("Entry detected\n");
-    // Rotate the servo to avoid the obstacle
-      myServo.attach(10);
-//      delay(1);
-//      myServo.write(0);      // Rotate servo to the left (adjust the angle as needed)
-//      delay(3000);         // Keep the servo in this position for 3 seconds
-//      myServo.write(80);     // Rotate servo back to the center position
-//      delay(1000);         // Delay before resuming normal operation
-     
-      
-      delay(2000);
-      for (int angle = 80; angle >=0; angle--) {
-        myServo.write(angle);
-        delay(40); // Adjust delay as needed for smoother motion
-      } 
-      delay(2000);
-        for (int angle = 0; angle <= 80; angle++) {
-        myServo.write(angle);
-        delay(40); // Adjust delay as needed for smoother motion
-      }
-       
+  // Check and send status of each parking slot in real time
+  checkAndSendSlotStatus(irSensorValue1, "1");
+  checkAndSendSlotStatus(irSensorValue2, "2");
+  checkAndSendSlotStatus(irSensorValue3, "3");
 
-
-      
-      myServo.detach();      // Detach servo to save power
-
-      if (irSensorValue1 == HIGH) {
-        Serial.println("Slot 1 Empty");
-        bluetooth.println("Slot 1 Empty"); // Sending '1' when Vehicle Parked at Slot  1
-      } else {
-        Serial.println("Vehicle Parked at Slot 1");
-        bluetooth.println("Vehicle Parked at Slot 1"); // Sending '0' when Empty Slot  1
-      }
+  // Check if obstacle is detected by IR sensor 4 i.e. gate
+  if (irSensorValue4 == HIGH) { 
+    Serial.println("No Entry detected");
+    bluetooth.println("No Entry detected");
+  } 
+  else {
+    Serial.println("Entry detected");
+    bluetooth.println("Entry detected");
     
-      if (irSensorValue2 == HIGH) {
-        Serial.println("Slot 2 Empty");
-        bluetooth.println("Slot 2 Empty"); // Sending '1' when Vehicle Parked at Slot  2
-      } else {
-        Serial.println("Vehicle Parked at Slot 2");
-        bluetooth.println("Vehicle Parked at Slot 2"); // Sending '0' when Empty Slot  2
-      }
-    
-      if (irSensorValue3 == HIGH) {
-        Serial.println("Slot 3 Empty");
-        bluetooth.println("Slot 3 Empty"); // Sending '1' when Vehicle Parked at Slot  3
-      } else {
-        Serial.println("Vehicle Parked at Slot 3");
-        bluetooth.println("Vehicle Parked at Slot 3"); // Sending '0' when Empty Slot  3
-      }
+    myServo.attach(10); // Attach servo to control gate
+    delay(2000);  
+
+    check(); //optional function call
+
+    //open gate slowly
+    for (int angle = 80; angle >= 0; angle--) {
+      myServo.write(angle);
+      delay(40);
+    }
+
+    //keep gate open until the vehicle is waiting at gate
+    while (irSensorValue4 != HIGH) {
+      irSensorValue4 = digitalRead(irSensorPin4); // Do nothing and keep checking irSensorValue4
+      check();
+      delay(100); // Adjust delay as needed
+    }
+
+    //close gate slowly
+    delay(2000);
+    for (int angle = 0; angle <= 80; angle++) {
+      myServo.write(angle);
+      delay(40);
+    }
+    myServo.detach(); // Detach servo to save power
   }
-  delay(5000); //scanning delay
-  Serial.println("____________________________");
-  bluetooth.println("____________________________");
+
+  check(); //optional function call
+  delay(500); // refresh frequency (500 ms)
+  
+  // separator for every reading
+  //Serial.println("________________________");
+  bluetooth.println("________________________"); 
+
+  // Receive messages from Bluetooth slave
   String receivedMessage = "";
   while (bluetooth.available() > 0) {
     char receivedChar = bluetooth.read();
+    check(); //optional function call
     if (receivedChar == '\n') {
       Serial.print("Received from slave: ");
       Serial.println(receivedMessage);
-      receivedMessage = ""; // Clear the message buffer
-    } else {
+      receivedMessage = "";
+    } 
+    else {
       receivedMessage += receivedChar;
       Serial.print("Received from slave (else): ");
       Serial.println(receivedMessage);
     }
   }
+}
+
+
+// Function to check and send status of the parking slots and
+// prints slot information in serial monitor which is then 
+// read by the python program running in the background 
+// and display information in the flask web app
+
+void checkAndSendSlotStatus(int irSensorValue, const char* slotName) {
+  if (irSensorValue == HIGH) {
+    Serial.print("Empty "); 
+    Serial.println(slotName);
+
+    bluetooth.print("Slot Empty at Slot ");
+    bluetooth.println(slotName);
+  } 
+  else {
+    Serial.print("Parked ");
+    Serial.println(slotName);
+
+    bluetooth.print("Vehicle Parked at Slot ");
+    bluetooth.println(slotName);
+  }
+}
+
+//check for parking lot slot status
+void check(){
+  irSensorValue1 = digitalRead(irSensorPin1);
+  irSensorValue2 = digitalRead(irSensorPin2);
+  irSensorValue3 = digitalRead(irSensorPin3);
+
+  checkAndSendSlotStatus(irSensorValue1, "1");
+  checkAndSendSlotStatus(irSensorValue2, "2");
+  checkAndSendSlotStatus(irSensorValue3, "3");
 }
